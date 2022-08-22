2e2ff591eee1203a6ad04273ebab6b5cbc5b8554
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-08-22 01:00:53 +0100

    i dey learn ooh😭

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 5, 2022 00:00:00").getTime();
-console.log(countDownDate);
+// console.log(countDownDate);
+var now = new Date().getTime();
+var remainingTime = countDownDate - now;
+let epochTime = countDownDate / 1000;
+// let remainingTimeInSecs = remainingTime / 1000;
 
 // Update the count down every 1 second
-var countdownUpdate = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
+// var countdownUpdate = setInterval(function () {
+//   // Get today's date and time
 
-  // Find the time remaining between now and the count down date
-  var remainingTime = countDownDate - now;
+//   // Find the time remaining between now and the count down date
 
-  // Time calculations for days, hours, minutes and seconds
-  var calculatedDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-  var calculatedHours = Math.floor(
-    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  var calculatedMinutes = Math.floor(
-    (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-  );
-  var calculatedSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var calculatedDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+//   var calculatedHours = Math.floor(
+//     (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//   );
+//   var calculatedMinutes = Math.floor(
+//     (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+//   );
+//   var calculatedSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-  //   // Output the result in an element with id="demo"
-  //   document.getElementById("demo").innerHTML =
-  //     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+//   //Output our results
+//   var days = document.querySelector(".days");
+//   var hours = document.querySelector(".hours");
+//   var minutes = document.querySelector(".minutes");
+//   var seconds = document.querySelector(".seconds");
 
-  //Output our results
-  var days = document.querySelector(".days");
-  var hours = document.querySelector(".hours");
-  var minutes = document.querySelector(".minutes");
-  var seconds = document.querySelector(".seconds");
+//   days.innerHTML = calculatedDays;
+//   hours.innerHTML = calculatedHours;
+//   minutes.innerHTML = calculatedMinutes;
+//   seconds.innerHTML = calculatedSeconds;
 
-  days.innerHTML = calculatedDays;
-  hours.innerHTML = calculatedHours;
-  minutes.innerHTML = calculatedMinutes;
-  seconds.innerHTML = calculatedSeconds;
+//   // If the count down is over, write some text
+//   if (remainingTime < 0) {
+//     clearInterval(countdownUpdate);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
 
-  // If the count down is over, write some text
-  if (remainingTime < 0) {
-    clearInterval(countdownUpdate);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+// 1667606400
+
+// let timer_ = 16000819919;
+// new Flipdown(timer_).start();
+// console.log(remainingTime);
+// console.log(`1667689200`);
+// console.log(countDownDate / 1000);
+// new FlipDown(1667689200).start();
+new FlipDown(epochTime).start();
